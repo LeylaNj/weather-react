e6ae44d555fc1fabff16baf3e5d187c19519ee85
LeylaNj <126622311+LeylaNj@users.noreply.github.com>
2023-09-11 00:14:28 +0200

    weather react

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Footer() {
+  return (
+    <div>
+      <span>Open source coded by </span>
+      <a href="#">Leyla Najafi </a>
+    </div>
+  );
+}
